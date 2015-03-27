daca8ded8f068404093c985fda7a7770ba24c659
Daniel Karlsson <daniel.karlsson@frojd.se>
2015-03-27 09:07:41 +0100

    Added context processors and context request to base.py for access to request in templates

--- django_boilerplate/django_boilerplate/settings/base.py
+++ django_boilerplate/django_boilerplate/settings/base.py
@@ -12,22 +12,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Version, be sure to bump this with each release
-APP_VERSION = "0.0.1"
+APP_VERSION = '0.0.1'
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if get_env_variable("DEBUG") == "True" else False
+DEBUG = True if get_env_variable('DEBUG') == 'True' else False
 
 # Minified, by default it is set to the same as Debug
 MINIFIED = DEBUG
 
 # This is when debug is off, else django wont allow you to visit the site
-ALLOWED_HOSTS = get_env_variable("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = get_env_variable('ALLOWED_HOSTS').split(',')
 
 INTERNAL_IPS = (
-    "127.0.0.1",
+    '127.0.0.1',
 )
 
 # Application definition
@@ -51,14 +51,14 @@ MIDDLEWARE_CLASSES = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.request",
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
     )
 
 # Project name is set in .env
@@ -71,34 +71,27 @@ WSGI_APPLICATION = '%s.wsgi.application' % PROJECT_NAME
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # Using PostgreSQL
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": get_env_variable("DB_NAME"),
-        "USER": get_env_variable("DB_USERNAME"),
-        "PASSWORD": get_env_variable("DB_PASSWORD"),
-        "HOST": get_env_variable("DB_HOST"),
-        "PORT": get_env_variable("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USERNAME'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': get_env_variable('DB_HOST'),
+        'PORT': get_env_variable('DB_PORT'),
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'sv-SE'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-STATIC_URL = '/static/'
 
 # Uploaded media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = get_env_variable("MEDIA_PATH")
+MEDIA_ROOT = get_env_variable('MEDIA_PATH')
 
 # Templates
 TEMPLATE_DEBUG = True
@@ -107,13 +100,15 @@ TEMPLATE_DIRS = (
 )
 
 # Static files, if in production use static root, else use static dirs
+STATIC_URL = '/static/'
+
 if not DEBUG:
-    STATIC_ROOT = get_env_variable("STATIC_PATH")
+    STATIC_ROOT = get_env_variable('STATIC_PATH')
 else:
     STATICFILES_DIRS = (
-        get_env_variable("STATIC_PATH"),
+        get_env_variable('STATIC_PATH'),
     )
 
 # Static folder for gulp/grunt generated files
 STATIC_FOLDER = 'builds'
-GA_ACCOUNT = "UA-xxxxxxx"
+GA_ACCOUNT = 'UA-xxxxxxx'
