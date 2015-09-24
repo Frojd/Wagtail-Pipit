c49ba8a813784d2a838404ace3bff10606cb35eb
Martin Sandström <martin@marteinn.se>
2015-09-24 14:37:43 +0200

    Added basic view test example

--- src/pages/tests.py
+++ src/pages/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from models import Page
 
-# Create your tests here.
+
+# Interested in tests? Awesome! Make sure you read this first:
+#   https://docs.djangoproject.com/en/dev/intro/tutorial05/
+
+
+class ViewTests(TestCase):
+    def test_empty_start_endpoint(self):
+        response = self.client.get(reverse('startpage'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_start_endpoint(self):
+        page = Page(title='About', slug='about', content='Hello world!')
+        page.save()
+
+        response = self.client.get(reverse('startpage'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'About')
