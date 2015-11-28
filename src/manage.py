6d37af0f05354098e961de03f8ae7114b7e6f636
Martin Sandström <martin@marteinn.se>
2015-11-28 17:59:59 +0100

    Changed from django_boilerplate to core app

--- src/manage.py
+++ src/manage.py
@@ -15,6 +15,6 @@ except Exception, e:
     pass
 
 if __name__ == "__main__":
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_boilerplate.settings.local")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
