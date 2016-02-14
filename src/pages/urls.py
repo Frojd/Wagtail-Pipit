2ee079109c18d008ca5c05d19c6b455ebff1505b
Martin Sandström <martin@marteinn.se>
2016-02-14 11:29:54 +0100

    Replaced pattern with list

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, url
 from views import StartView
 
 
-urlpatterns = patterns('pages.views',
+urlpatterns = [
     # Some basic examples
     # Root startpage eg. http://site.com
     url(r'^$', StartView.as_view(), name='startpage'),
 
     # Slugged page eg. http://site.com/subpage/
     url(r'^(?P<slug_value>[\w\-]+)/$', 'subpage', name='subpage'),
-)
+]
