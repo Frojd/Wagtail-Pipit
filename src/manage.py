258680a1c3fb70d9ad39c6bbf0d5a4f5e10adab9
Martin Sandström <martin@marteinn.se>
2016-11-29 20:08:14 +0100

    Muted env file warnings (to avoid docker noise)

--- src/manage.py
+++ src/manage.py
@@ -12,7 +12,10 @@ from django.core.management import execute_from_command_line
 try:
     inspect_file = inspect.getfile(inspect.currentframe())
     env_path = os.path.dirname(os.path.abspath(inspect_file))
-    dotenv.load_dotenv("%s/.env" % (env_path,))
+    env_file = "{}/.env".format(env_path,)
+
+    if os.path.exists(env_file):
+        dotenv.load_dotenv(env_file)
 except Exception as e:
     pass
 
