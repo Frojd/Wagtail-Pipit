91d3f255e505e7aa5995d5a05d76572955c6b77d
Martin Sandström <martin@marteinn.se>
2017-01-19 09:46:34 +0100

    Updated django debug toolbar implementation in local

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 
@@ -10,4 +10,12 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # Prefered way of including an apps urls
     # url(r'', include('pages.urls'))  # Uncomment this to use the example app
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
