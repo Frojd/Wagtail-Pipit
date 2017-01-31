8978d7f1cd4b034deaa06f9a070947ea2efee753
Martin Sandström <martin@marteinn.se>
2017-01-31 20:55:21 +0100

    Added working django-debug-toolbar under docker

--- src/core/settings/local.py
+++ src/core/settings/local.py
@@ -8,10 +8,31 @@ Write local settings here, or override base settings
 from base import *  # NOQA
 
 
+DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
 # Add django debug toolbar when using local version
-INSTALLED_APPS += (
+INSTALLED_APPS += [
     'debug_toolbar',
-)
+]
+
+MIDDLEWARE_CLASSES += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
 
 # Allow weak local passwords
 AUTH_PASSWORD_VALIDATORS = []
+
+INTERNAL_IPS = get_env_variable('INTERNAL_IPS', default="").split(',')
+
+
+# Allow django-debug-bar under docker
+def show_toolbar(request):
+    # https://gist.github.com/douglasmiranda/9de51aaba14543851ca3
+    return not request.is_ajax()
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'core.settings.local.show_toolbar',
+}
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import url, include
-from django.contrib import admin
 from django.conf.urls.static import static
 
 
