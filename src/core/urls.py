598c2390d8a521b07324d2bd6116b8b8d306c44e
Martin Sandström <martin@marteinn.se>
2017-03-16 19:48:55 +0100

    Added extra check to make sure django_toolbar is installed

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,9 +13,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns += [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
