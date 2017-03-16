0ad850bd8d06dd37e9b8cd8026ad0149133a02e3
Martin Sandström <martin@marteinn.se>
2017-03-16 19:49:44 +0100

    Added views for debugging 400-500 exceptions

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.views import defaults as default_views
 
 
 urlpatterns = [
@@ -13,8 +14,16 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += [
+        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),  # NOQA
+        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),  # NOQA
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),  # NOQA
+        url(r'^500/$', default_views.server_error),
+    ]
+
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
+
         urlpatterns += [
             url(r'^__debug__/', include(debug_toolbar.urls)),
         ]
