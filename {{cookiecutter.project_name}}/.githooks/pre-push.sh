19a646dcd94f441da7ddf1b37a60369f3ae0c75b
Martin Sandström <martin@marteinn.se>
2017-04-18 20:24:23 +0200

    Simplified pre-push hook

--- {{cookiecutter.project_name}}/.githooks/pre-push.sh
+++ {{cookiecutter.project_name}}/.githooks/pre-push.sh
@@ -7,17 +7,6 @@
 # $1 -- Name of the remote to which the push is being done
 # $2 -- URL to which the push is being done
 
+docker-compose run web test --rm
 
-# Django tests
-eval $(docker-machine env default)
-
-TEST_CONTAINER="<project_prefix>_web_1"
-
-if [ "$(docker ps | grep <project_prefix>_web_run_1)" ]; then
-    TEST_CONTAINER="<project_prefix>_web_run_1"
-fi
-
-git diff --cached --name-only | if grep --quiet "src/"
-then
-    docker exec -i $TEST_CONTAINER python manage.py test --settings=core.settings.test --noinput
-fi
+exit $?
