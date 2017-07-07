40c88927dbdcf7c527267b714f2bac86708784f0
Martin Sandström <martin@marteinn.se>
2017-07-07 07:47:07 +0200

    Remove .env reading when running locally

--- {{cookiecutter.project_name}}/src/manage.py
+++ {{cookiecutter.project_name}}/src/manage.py
@@ -9,16 +9,6 @@ import dotenv
 from django.core.management import execute_from_command_line
 
 
-try:
-    inspect_file = inspect.getfile(inspect.currentframe())
-    env_path = os.path.dirname(os.path.abspath(inspect_file))
-    env_file = "{}/.env".format(env_path,)
-
-    if os.path.exists(env_file):
-        dotenv.load_dotenv(env_file)
-except Exception as e:
-    pass
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
     execute_from_command_line(sys.argv)
