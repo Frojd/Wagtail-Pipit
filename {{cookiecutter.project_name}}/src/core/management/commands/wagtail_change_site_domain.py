23ad03614c42558c4008ceb83779af3ed10d89bf
Martin Sandström <martin@marteinn.se>
2017-10-03 22:32:49 +0200

    Remove unecessary logger from management commands

--- {{cookiecutter.project_name}}/src/core/management/commands/wagtail_change_site_domain.py
+++ {{cookiecutter.project_name}}/src/core/management/commands/wagtail_change_site_domain.py
@@ -1,10 +1,5 @@
-import logging
-
 from django.core.management.base import BaseCommand
 from wagtail.wagtailcore.models import Site
-
-
-logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
