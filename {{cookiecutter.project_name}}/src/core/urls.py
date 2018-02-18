79619264b49378dbcd44e10e8c5048e117ea9922
Martin Sandström <martin@marteinn.se>
2018-02-18 11:31:35 +0100

    More wagtail 2.0 path fixes

--- {{cookiecutter.project_name}}/src/core/urls.py
+++ {{cookiecutter.project_name}}/src/core/urls.py
@@ -11,7 +11,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.search import urls as wagtailsearch_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
-from wagtail.contrib.wagtailsitemaps.views import sitemap
+from wagtail.contrib.sitemaps.views import sitemap
 {% endif %}
 
 
