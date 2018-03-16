8e0e52787cda38b4947a6833d799ad83cd72a4a0
Martin Sandström <martin@marteinn.se>
2018-03-16 14:24:44 +0100

    Add page stub models for wagtail

---
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/models/base.py
@@ -0,0 +1,13 @@
+from wagtail.core.models import Page
+from django_react_templatetags.mixins import RepresentationMixin
+
+
+class Base(RepresentationMixin, SeoMixin, Page):
+    # Basepage is not anything creatable in admin
+    is_creatable = False
+    show_in_menus_default = True
+
+    def __init__(self, *args, **kwargs):
+        self.template = 'pages/react.html'
+        self.component_name = 'Page{}'.format(self.__class__.__name__)
+        super().__init__(*args, **kwargs)
