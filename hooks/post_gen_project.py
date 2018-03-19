db2f279ce72b682b89e3a2a0fee16f6b109b25bf
rinti <andreas.bernacca@frojd.se>
2018-03-19 14:20:24 +0100

    Add option to install react-sass-starterkit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -9,6 +10,31 @@ shutil.copyfile(
     os.path.join(DOCKER_DIR, 'web.example.env'),
     os.path.join(DOCKER_DIR, 'web.env')
 )
+
+
+# Clone react sass starterkit and use latest tag
+if '{{ cookiecutter.use_wagtail }}'.lower() == 'y':
+    shutil.rmtree(os.path.join(
+        PROJECT_DIRECTORY, 'frontend'
+    ))
+
+    starterkit_repo = 'git@github.com:Frojd/react-sass-starterkit.git'
+
+    # Clone starter kit into ./frontend
+    p = subprocess.Popen(['git', 'clone', starterkit_repo, 'frontend'])
+    p.communicate()
+
+    # Get the name of the latest tag
+    tag = subprocess.check_output('cd frontend && git describe --tags $(git rev-list --tags --max-count=1)', shell=True)
+
+    # Fetch the tags and checkout latest tag
+    p = subprocess.Popen('cd frontend && git fetch --tags && git checkout tags/{}'.format(tag.decode('utf-8')), shell=True)
+    p.communicate()
+
+    # Remove git repository from frontend
+    shutil.rmtree(os.path.join(
+        PROJECT_DIRECTORY, 'frontend/.git'
+    ))
 
 
 # Remove sites framework file (if wagtail is in use)
