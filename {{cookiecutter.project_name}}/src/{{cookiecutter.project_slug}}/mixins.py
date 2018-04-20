fa9fbfb1feae3c24b531fbb457e52801e8418c92
rinti <andreas.bernacca@frojd.se>
2018-04-20 08:55:08 +0200

    Add CreateUpdateModelMixin

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/mixins.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/mixins.py
@@ -106,3 +106,11 @@ class SeoMixin(Page):
 
     class Meta:
         abstract = True
+
+
+class CreateUpdateModelMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
