fac847bfe6e63edcec1ebddf0f8552247d64347a
Martin Sandström <martin@marteinn.se>
2018-08-09 13:11:07 +0200

    Add support for returning json for page instead of html

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/pages/base.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_slug}}/pages/base.py
@@ -14,3 +14,12 @@ class BasePage(RepresentationMixin, SeoMixin, Page):
         self.template = "pages/react.html"
         self.component_name = "{}Page".format(self.__class__.__name__)
         super().__init__(*args, **kwargs)
+
+    def serve(self, request, *args, **kwargs):
+        if request.content_type == "application/json":
+            from django.http import JsonResponse
+
+            json = self.to_react_representation({"request": request})
+            return JsonResponse(json)
+
+        return super().serve(request, *args, **kwargs)
