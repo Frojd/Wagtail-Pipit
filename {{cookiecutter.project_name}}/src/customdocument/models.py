c1bbcb131657b9bf4b0757ccedc586f5b6bdffe4
Martin Lindvall <martin.lindvall@frojd.se>
2018-09-28 13:08:57 +0200

    Add cusom user and document models

---
+++ {{cookiecutter.project_name}}/src/customdocument/models.py
@@ -0,0 +1,8 @@
+from wagtail.documents.models import Document, AbstractDocument
+
+class CustomDocument(AbstractDocument):
+    # Custom field example:
+
+    admin_form_fields = Document.admin_form_fields + (
+        # Add all custom fields names to make them appear in the form:
+    )
