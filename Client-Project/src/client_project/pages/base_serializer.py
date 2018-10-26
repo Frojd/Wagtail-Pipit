9c4abd6962585dfbbafb93b65134b983191463b9
Martin Sandström <martin@marteinn.se>
2018-10-26 12:02:18 +0200

    Add SiteSetting serializer to BasePageSerializer

--- Client-Project/src/client_project/pages/base_serializer.py
+++ Client-Project/src/client_project/pages/base_serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from wagtail.core import fields
 from wagtail.api.v2 import serializers as wagtail_serializers
 
+from sitesettings.models import SiteSetting
+from sitesettings.serializers import SiteSettingSerializer
 from . import BasePage
 
 
@@ -12,6 +14,8 @@ class BasePageSerializer(serializers.ModelSerializer):
     serializer_field_mapping.update(
         {fields.StreamField: wagtail_serializers.StreamField}
     )
+
+    site_setting = serializers.SerializerMethodField()
 
     class Meta:
         model = BasePage
@@ -31,4 +35,16 @@ class BasePageSerializer(serializers.ModelSerializer):
             "twitter_image",
             "robot_noindex",
             "robot_nofollow",
+
+            # from external relation
+            "site_setting",
         ]
+
+    def get_site_setting(self, page):
+        request = self.context.get('request', None)
+
+        if not request:
+            return None
+
+        site_setting = SiteSetting.for_site(request.site)
+        return SiteSettingSerializer(site_setting).data
