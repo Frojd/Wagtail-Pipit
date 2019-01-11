663d2273856073418dbc7bd87c2ac6a28bc261c5
Daniel Karlsson <daniel.karlsson@frojd.se>
2019-01-11 15:10:37 +0100

    Fix ssr

--- {{cookiecutter.project_name}}/frontend/babel.config.js
+++ {{cookiecutter.project_name}}/frontend/babel.config.js
@@ -9,6 +9,7 @@ const presets = [
 const plugins = [
     'react-hot-loader/babel',
     'babel-plugin-webpack-aliases',
+    'require-context-hook',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
 ]
--- {{cookiecutter.project_name}}/frontend/internals/example/containers/App/App.js
+++ {{cookiecutter.project_name}}/frontend/internals/example/containers/App/App.js
@@ -1,3 +1,5 @@
+require('babel-plugin-require-context-hook/register')();
+
 import React, { PureComponent } from 'react';
 import { keysToCamelFromSnake } from '../../utils/caseconverters';
 import './App.scss';
