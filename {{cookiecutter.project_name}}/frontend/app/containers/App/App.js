e91d783c56baffdb82e590cbc02d67579ce7d8fe
Daniel Karlsson <daniel.karlsson@frojd.se>
2019-01-11 13:12:01 +0100

    Update app.js with better resourcemanagement

--- {{cookiecutter.project_name}}/frontend/app/containers/App/App.js
+++ {{cookiecutter.project_name}}/frontend/app/containers/App/App.js
@@ -2,6 +2,14 @@ import React, { PureComponent } from 'react';
 import { keysToCamelFromSnake } from '../../utils/caseconverters';
 import './App.scss';
 
+const cache = {};
+
+function importAll(r) {
+    r.keys().forEach(key => cache[key] = r(key));
+}
+
+importAll(require.context('../../containers', true, /index.js$/));
+
 class App extends PureComponent {
 
     render() {
@@ -14,9 +22,9 @@ class App extends PureComponent {
         if(!componentName) {
             return false;
         }
-        
-        const Component = require(`../../containers/${componentName}`).default;
-        
+
+        const Component = cache[componentName];
+
         return (
             <Component {...keysToCamelFromSnake(componentProps)} />
         );
