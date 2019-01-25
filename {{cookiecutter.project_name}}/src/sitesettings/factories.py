9b3bdaadc55b23e91bf058bc028782ed22ed8b5f
Martin Sandström <martin@marteinn.se>
2019-01-25 16:02:16 +0100

    Add site setting factory

---
+++ {{cookiecutter.project_name}}/src/sitesettings/factories.py
@@ -0,0 +1,12 @@
+import factory
+
+from wagtail_factories import SiteFactory
+
+from .models import SiteSetting
+
+
+class SiteSettingFactory(factory.DjangoModelFactory):
+    site = factory.SubFactory(SiteFactory)
+
+    class Meta:
+        model = SiteSetting
