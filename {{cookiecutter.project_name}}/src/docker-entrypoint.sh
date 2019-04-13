befd8446a95fb569768eb9217a318eab205e03dd
Martin Sandström <martin@marteinn.se>
2019-04-13 11:45:53 +0200

    Add error message if ssl certs are missing in runserver_ssl

--- {{cookiecutter.project_name}}/src/docker-entrypoint.sh
+++ {{cookiecutter.project_name}}/src/docker-entrypoint.sh
@@ -50,6 +50,13 @@ case "$CMD" in
         wait_for_db
         setup_django
 
+        if [ ! -f "/priv/cert/cert-key.pem" ]; then
+            echo "Error! You are missing the required SSL certificates"
+            echo "To solve it, make sure you have mkcert installed with a root cert, then run:"
+            echo "mkcert --cert-file docker/files/certs/cert.pem --key-file docker/files/certs/cert-key.pem {{cookiecutter.domain_prod}}.test"
+            exit 1
+        fi
+
         echo Starting using manage.py runsslserver
         exec python manage.py runsslserver 0.0.0.0:8000 --certificate /priv/cert/cert.pem --key /priv/cert/cert-key.pem
         ;;
