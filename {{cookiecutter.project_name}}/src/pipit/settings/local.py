5433714ab0d7e5548b7712f6651d0d0e38e3fa4c
Martin Sandström <martin@marteinn.se>
2019-06-06 07:58:54 +0200

    Make debug toolbar optional

--- {{cookiecutter.project_name}}/src/pipit/settings/local.py
+++ {{cookiecutter.project_name}}/src/pipit/settings/local.py
@@ -8,28 +8,31 @@ from __future__ import absolute_import, unicode_literals
 
 from pipit.settings.base import *  # NOQA
 
+
 VS_CODE_REMOTE_DEBUG = get_env_bool("VS_CODE_REMOTE_DEBUG", default=False)
 DEBUG = True
 TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
 
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-# Add django debug toolbar when using local version
-INSTALLED_APPS += ["debug_toolbar", "sslserver"]
-
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+# Replace built in runserver with a ssl enabled
+INSTALLED_APPS += ["sslserver"]
 
 # Allow weak local passwords
 AUTH_PASSWORD_VALIDATORS = []
 
 INTERNAL_IPS = get_env("INTERNAL_IPS", default="").split(",")
 
+# Add django debug toolbar when using local version
+if get_env_bool("DEBUG_TOOLBAR", default=False):
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+    INSTALLED_APPS += ["debug_toolbar"]
+
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+    DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": "pipit.settings.local.show_toolbar"}
 
 # Allow django-debug-bar under docker
 def show_toolbar(request):
     return True
-
-
-DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": "pipit.settings.local.show_toolbar"}
