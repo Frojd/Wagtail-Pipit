657d6a3949fdc8a1bd72880a0e0e2776e475ae5d
Martin Sandström <martin@marteinn.se>
2020-03-23 21:04:03 +0100

    Add user reporting on 500 view

---
+++ {{cookiecutter.project_name}}/src/main/views/error_500.py
@@ -0,0 +1,8 @@
+from sentry_sdk import last_event_id
+from django.shortcuts import render
+
+
+def error_500_view(request, *args, **argv):
+    return render(request, "500.html", {
+        'sentry_event_id': last_event_id(),
+    }, status=500)
