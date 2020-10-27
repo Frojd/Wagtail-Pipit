6625f1652eed20d01634b63874aad0df54f851de
Martin Sandström <martin@marteinn.se>
2020-10-27 19:31:35 +0100

    Rebuild project

--- Company-Project/.githooks/bump-version.sh
+++ Company-Project/.githooks/bump-version.sh
@@ -21,9 +21,9 @@ VERSION=${VERSION#"v"}
 ROOTDIR=$(git rev-parse --show-toplevel)
 
 # Bump django version
-sed -i.bak 's/^APP_VERSION.=.*/APP_VERSION = "'$VERSION'"/' $ROOTDIR/src/core/settings/base.py
+sed -i.bak 's/^APP_VERSION.=.*/APP_VERSION = "'$VERSION'"/' $ROOTDIR/src/pipit/settings/base.py
 
-rm src/core/settings/base.py.bak
+rm src/pipit/settings/base.py.bak
 
 # Bump package.version
 sed -i.bak 's/^\( *\)"version": .*/\1"version": "'$VERSION'",/' $ROOTDIR/frontend/package.json
