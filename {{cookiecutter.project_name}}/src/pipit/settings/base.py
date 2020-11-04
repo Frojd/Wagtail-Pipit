6880dbfdd51f610f33860b666459ac4308148c56
Martin Sandström <martin@marteinn.se>
2020-11-04 20:20:19 +0100

    Streamline S3 config and improve boto -> boto3 migration

--- {{cookiecutter.project_name}}/src/pipit/settings/base.py
+++ {{cookiecutter.project_name}}/src/pipit/settings/base.py
@@ -1,8 +1,6 @@
 import os
 from typing import Optional
 
-from boto.s3.connection import OrdinaryCallingFormat, S3Connection
-
 from pipit.settings import get_env, get_env_bool
 
 
@@ -169,16 +167,11 @@ if get_env("AWS_ACCESS_KEY_ID", ""):
     AWS_SECRET_ACCESS_KEY = get_env("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = get_env("AWS_BUCKET_NAME")
 
-    AWS_AUTO_CREATE_BUCKET = True
     AWS_QUERYSTRING_AUTH = False
-    AWS_EXPIRY = 60 * 60 * 24 * 7
     AWS_S3_FILE_OVERWRITE = False
 
+    AWS_EXPIRY = 60 * 60 * 24 * 7  # One week
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age={}".format(AWS_EXPIRY)}
-    AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
-
-    # Retrieve S3 files using https, with a bucket that contains a dot.
-    S3Connection.DefaultHost = "s3-eu-west-1.amazonaws.com"
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     THUMBNAIL_DEFAULT_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
