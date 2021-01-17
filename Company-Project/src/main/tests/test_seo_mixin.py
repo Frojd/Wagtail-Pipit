fe09b4d333823df4f6681f6fa0db5db510a0d5cd
Martin Sandström <martin@marteinn.se>
2021-01-17 21:48:31 +0100

    Fix failing test in seo mixin

--- Company-Project/src/main/tests/test_seo_mixin.py
+++ Company-Project/src/main/tests/test_seo_mixin.py
@@ -24,7 +24,7 @@ class SeoMixinTest(WagtailPageTests):
         )
 
     def test_common_fields(self):
-        self.assertEqual(self.page.seo_html_title, "My start")
+        self.assertEqual(self.page.seo_html_title, "My start | Test site")
         self.assertEqual(self.page.seo_meta_description, "Search description")
         self.assertEqual(self.page.seo_og_title, "My og start")
         self.assertEqual(self.page.seo_og_description, "My og description")
