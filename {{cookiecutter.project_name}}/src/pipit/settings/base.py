8d68e828c2f3adb378b9eb955be6f18bde524f08
Martin Sandström <martin@marteinn.se>
2021-03-20 19:05:09 +0100

    Drop unused redirect middleware. Fixes #371

--- {{cookiecutter.project_name}}/src/pipit/settings/base.py
+++ {{cookiecutter.project_name}}/src/pipit/settings/base.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
 ROOT_URLCONF = "pipit.urls"
