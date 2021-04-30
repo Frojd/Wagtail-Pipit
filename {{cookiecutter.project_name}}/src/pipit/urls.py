04e252e0095c3065d585b8e7c1772a70c2078fa8
Martin Sandström <martin@marteinn.se>
2021-04-30 09:18:39 +0200

    Drop type ignores by createing custom urlpattern type alias

--- {{cookiecutter.project_name}}/src/pipit/urls.py
+++ {{cookiecutter.project_name}}/src/pipit/urls.py
@@ -1,6 +1,8 @@
+import typing
+
 from django.contrib import admin
 from django.conf import settings
-from django.urls import include, path, re_path
+from django.urls import include, path, re_path, URLResolver, URLPattern
 from django.conf.urls.static import static
 from django.views import defaults as default_views
 from wagtail.admin import urls as wagtailadmin_urls
@@ -12,11 +14,13 @@ from main.views.page_not_found import PageNotFoundView
 from main.views.error_500 import error_500_view
 from nextjs.api import api_router
 
-
 handler404 = PageNotFoundView.as_view()
 handler500 = error_500_view
 
-urlpatterns = []
+URL = typing.Union[URLPattern, URLResolver]
+URLList = typing.List[URL]
+
+urlpatterns: URLList = []
 
 if settings.DEBUG:
     urlpatterns += [
@@ -41,16 +45,16 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns += [path("wt/__debug__/", include(debug_toolbar.urls))]  # type: ignore
+        urlpatterns += [path("wt/__debug__/", include(debug_toolbar.urls))]
 
 urlpatterns += [
     path(settings.ADMIN_URL, admin.site.urls),
     path("wt/api/nextjs/v1/", api_router.urls),
-    path("wt/cms/", include(wagtailadmin_urls)),  # type: ignore
-    path("wt/documents/", include(wagtaildocs_urls)),  # type: ignore
+    path("wt/cms/", include(wagtailadmin_urls)),
+    path("wt/documents/", include(wagtaildocs_urls)),
     path("wt/sitemap.xml", sitemap, name="sitemap"),
 ]
 
-urlpatterns += [re_path(r"", include(wagtail_urls))]  # type: ignore
+urlpatterns += [re_path(r"", include(wagtail_urls))]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
