27c743619eb238b815019ae9e6b8d4a40bcccf37
Martin Sandström <martin@marteinn.se>
2021-05-25 20:18:03 +0200

    Indicate that arg is unused

--- Company-Project/src/customimage/factories.py
+++ Company-Project/src/customimage/factories.py
@@ -6,7 +6,7 @@ from .models import CustomImage
 
 class CustomImageFactory(factory.DjangoModelFactory):
     title = factory.sequence(lambda x: "extended-image-{0}".format([x]))
-    file = factory.LazyAttribute(lambda x: get_test_image_file())
+    file = factory.LazyAttribute(lambda _: get_test_image_file())
 
     class Meta:
         model = CustomImage
