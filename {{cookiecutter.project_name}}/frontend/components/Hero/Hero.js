c89249e9f7e1a786294a76aa1747ba1f5d56bc7c
Andreas Bernacca <hello@rinti.se>
2021-12-22 21:31:44 +0100

    Update packages and add Svg to hero, to test that imported SVGs works

--- {{cookiecutter.project_name}}/frontend/components/Hero/Hero.js
+++ {{cookiecutter.project_name}}/frontend/components/Hero/Hero.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Hero.module.css';
+import Logo from '../../public/img/logo.svg'
 
 const Hero = ({ title }) => (
     <div className={s.Container}>
+        <Logo className={s.Logo} />
         <h1 className={s.Title}>
             <img
                 src={'/img/white_circle.png'}
