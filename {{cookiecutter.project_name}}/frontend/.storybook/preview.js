9ce5d0dbbe601f28fd10d464c89ee6666d2d89a6
Andreas Bernacca <hello@rinti.se>
2022-11-15 20:42:26 +0100

    Upgrade frontend to utilize new storybook conventions and next-i18next, and upgrade packages

--- {{cookiecutter.project_name}}/frontend/.storybook/preview.js
+++ {{cookiecutter.project_name}}/frontend/.storybook/preview.js
@@ -1,25 +1,52 @@
-import { addParameters } from '@storybook/client-api';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 import '../styles/index.css';
 
+export const decorators = [
+  (storyFn) => <I18nextProvider i18n={i18n}>{storyFn()}</I18nextProvider>,
+];
+
 const customViewports = {
-    gridSL: {
-        name: 'GRID - SL - Fluid',
-        styles: {
-            width: '500px',
-            height: '950px',
-        },
+  gridSL: {
+    name: 'GRID - SL - Fluid',
+    styles: {
+      width: '500px',
+      height: '950px',
     },
-    gridS: {
-        name: 'GRID - S - Fluid',
-        styles: {
-            width: '375px',
-            height: '950px',
-        },
+  },
+  gridS: {
+    name: 'GRID - S - Fluid',
+    styles: {
+      width: '375px',
+      height: '950px',
     },
+  },
 };
 
-addParameters({
-    viewport: { viewports: { ...customViewports, ...INITIAL_VIEWPORTS } },
-});
+export const parameters = {
+  layout: 'fullscreen',
+  viewport: { viewports: { ...customViewports, ...INITIAL_VIEWPORTS } },
+  backgrounds: {
+    default: 'white',
+    values: [
+      {
+        name: 'white',
+        value: '#fff',
+      },
+      {
+        name: 'primary pink',
+        value: '#c40064',
+      },
+      {
+        name: 'primary orange',
+        value: '#c9472d',
+      },
+      {
+        name: 'dark grey',
+        value: '#333',
+      },
+    ],
+  },
+};
--- {{cookiecutter.project_name}}/frontend/next.config.js
+++ {{cookiecutter.project_name}}/frontend/next.config.js
@@ -1,52 +1,51 @@
-const withPlugins = require('next-compose-plugins');
+const { i18n } = require('./next-i18next.config');
 
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const basePath = '';
 
 let nextConfig = {
-    webpack5: true,
-    trailingSlash: true,
-    productionBrowserSourceMaps: true,
-    basePath,
+  trailingSlash: true,
+  productionBrowserSourceMaps: true,
+  basePath,
+  i18n,
 };
 
 const withSvgr = (nextConfig = {}) => {
-    return Object.assign({}, nextConfig, {
-        webpack(config) {
-            config.module.rules.push({
-                test: /\.svg$/i,
-                issuer: /\.[jt]sx?$/,
-                use: [{ loader: '@svgr/webpack', options: { ref: true } }],
-            });
-            return config;
-        },
-    });
+  return Object.assign({}, nextConfig, {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+  });
 };
 
 const SentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-    silent: true,
-    dryRun: process.env.IGNORE_SENTRY ? true : false,
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true,
+  dryRun: process.env.IGNORE_SENTRY ? true : false,
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
-nextConfig = withSentryConfig(nextConfig, SentryWebpackPluginOptions);
+// nextConfig = withSentryConfig(nextConfig, SentryWebpackPluginOptions);
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //     enabled: process.env.ANALYZE === 'true',
 // });
 
-module.exports = withPlugins(
-    [
-        withSvgr,
-        //withBundleAnalyzer,
-    ],
-    nextConfig
-);
+module.exports = () => {
+  const plugins = [withSvgr];
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  });
+};
