5aded5b38d6c4e06b3d6232e9a28d542f3530dc4
Andreas Bernacca <hello@rinti.se>
2022-11-19 20:37:34 +0100

    Make images work in storybook

--- {{cookiecutter.project_name}}/frontend/.storybook/preview.js
+++ {{cookiecutter.project_name}}/frontend/.storybook/preview.js
@@ -1,6 +1,14 @@
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 import '../styles/index.css';
 
