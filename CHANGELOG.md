24209f567632a0754b376d99027d740edfca8269
Martin Sandström <martin@marteinn.se>
2023-05-19 15:36:26 +0200

    Upgrade storybook to v7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 - Make paths/imports better reflect scaffolder in Working with Wagtails routable pages (@DBonbon )
 - Upgrade next to 13.3.4 (@marteinn)
+- Upgrade storybook to v7 (@marteinn)
 
 ### Fixed
 - Replace deprecated STATICFILES_STORAGE setting with STORAGES (@marteinn)
--- {{cookiecutter.project_name}}/frontend/.storybook/main.js
+++ {{cookiecutter.project_name}}/frontend/.storybook/main.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
     staticDirs: ['../public'],
     stories: [
@@ -7,9 +5,6 @@ module.exports = {
         '../components/**/*.stories.js',
         '../containers/**/*.stories.js',
     ],
-    core: {
-        builder: 'webpack5',
-    },
     babel: async (options) => {
         options.presets = ['next/babel'];
         return {
@@ -20,32 +15,15 @@ module.exports = {
         '@storybook/addon-viewport',
         '@storybook/addon-a11y',
         '@storybook/addon-backgrounds',
-        {
-            name: 'storybook-addon-next',
-            options: {
-                nextConfigPath: path.resolve(__dirname, '../next.config.js'),
-            },
-        },
     ],
     features: {
         babelModeV7: true,
     },
-    webpackFinal: (config) => {
-        /**
-         * Fixes issue with `next-i18next` and is ready for webpack@5
-         * @see https://github.com/isaachinman/next-i18next/issues/1012#issuecomment-792697008
-         * @see https://github.com/storybookjs/storybook/issues/4082#issuecomment-758272734
-         * @see https://webpack.js.org/migrate/5/
-         */
-        config.resolve.fallback = {
-            fs: false,
-            tls: false,
-            net: false,
-            module: false,
-            path: require.resolve('path-browserify'),
-            crypto: false,
-        };
-
-        return config;
+    docs: {
+        autodocs: true,
+    },
+    framework: {
+        name: '@storybook/nextjs',
+        options: {},
     },
 };
