4e6d8d6b5924d564078c0aa06227c0fe7840303c
Martin Sandström <martin@marteinn.se>
2024-02-04 07:26:05 +0100

    Make sure whitenoise initialization are added to template

--- {{cookiecutter.project_name}}/src/pipit/settings/base.py
+++ {{cookiecutter.project_name}}/src/pipit/settings/base.py
@@ -63,7 +63,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
